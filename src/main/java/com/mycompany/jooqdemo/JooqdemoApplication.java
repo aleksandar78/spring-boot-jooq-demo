307539ab65c9290727814516fceb8bd2e714b2c7
Aleksandar Pavlovic <aleksandar.pavlovic78@gmail.com>
2018-09-25 21:35:50 +0200

    Generate spring boot project

---
+++ src/main/java/com/mycompany/jooqdemo/JooqdemoApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.jooqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqdemoApplication.class, args);
+	}
+}
