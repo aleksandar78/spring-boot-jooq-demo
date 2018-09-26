c2abf9b2298710c613dd5ff41f5026395216b96d
Aleksandar Pavlovic <aleksandar.pavlovic@exprivia.it>
2018-09-26 14:52:51 +0200

    Add bean factory for ExecuteListenerProvider

--- src/main/java/com/mycompany/jooqdemo/JooqdemoApplication.java
+++ src/main/java/com/mycompany/jooqdemo/JooqdemoApplication.java
@@ -1,7 +1,15 @@
 package com.mycompany.jooqdemo;
 
+import org.jooq.ExecuteListenerProvider;
+import org.jooq.impl.DefaultConfiguration;
+import org.jooq.impl.DefaultExecuteListenerProvider;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
 public class JooqdemoApplication {
@@ -9,4 +17,34 @@ public class JooqdemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JooqdemoApplication.class, args);
 	}
+	
+	// WHIT THIS CONFIGURATION ENABLED REST EXCEPTION HANDLER CATCH SPRING DATA ACCESS EXCEPTION ( BEHAVIOR NOT EXPECTED )
+	@Bean
+	@Order(Ordered.HIGHEST_PRECEDENCE)
+	public ExecuteListenerProvider customeListenerProvider() {
+		return new DefaultExecuteListenerProvider(new ApplicationExceptionTranslator());
+	}
+	
+	// WITH THIS CONFIGURATION ENABLED REST EXCEPTION HANDLER CATCH APPLICATION EXCEPTION ( BEHAVIOR EXPECTED )
+//	@Bean
+//	public static BeanPostProcessor jooqConfigurationPostProcessor() {
+//		return new BeanPostProcessor() {
+//
+//			@Override
+//			public Object postProcessBeforeInitialization(Object bean, String beanName)
+//					throws BeansException {
+//				return bean;
+//			}
+//
+//			@Override
+//			public Object postProcessAfterInitialization(Object bean, String beanName)
+//					throws BeansException {
+//				if (bean instanceof DefaultConfiguration) {
+//					((DefaultConfiguration) bean).set(new ApplicationExceptionTranslator());
+//				}
+//				return bean;
+//			}
+//
+//		};
+//	}
 }
